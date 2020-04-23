3f92bb8835e1c3b857b760d8c7cf52ad41d0fc54
Quang Dang <firesidecake@gmail.com>
2020-04-24 02:34:15 +1000

    Change sample reducer file name

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { sampleReducer } from './sample';
+import { sampleReducer } from './sampleReducer';
 
 const rootReducer = combineReducers({
   samples: sampleReducer,
